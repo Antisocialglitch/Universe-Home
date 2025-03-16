ba1674a1bc6cd1f1ac83e3dea0bbbea1e4e9aa6a
Antisocialglitch <okoroifeanyi39@gmail.com>
2025-03-16 12:52:55 +0100

    reserveFunc

--- script.js
+++ script.js
@@ -5,25 +5,22 @@ document.querySelector('h1')
 const firstImg = document.querySelector("img")
 firstImg.style.width ="350px"
 
-const sum = 0
-document.getElementById("guestAmount").onclick = function (){
-    sum++
-}
 
-document.getElementById("mButton").onclick = function(){
-    document.getElementById("text").innerHTML = "wagwan"
-}
+
+// document.getElementById("mButton").onclick = function(){
+//     document.getElementById("text").innerHTML = "wagwan"
+// }
 
 // document.getElementById("redButton").onclick = function(){
 //     document.getElementById("red").innerHTML = "wagwan"
 // }
 
-document.getElementById("red").onclick = function(){
-    document.getElementById("red").style.display = "none"
-}
+// document.getElementById("red").onclick = function(){
+//     document.getElementById("red").style.display = "none"
+// }
 
-
-function appendImage(imagePath){
+//RANDOS
+/*function appendImage(imagePath){
     const container = document.getElementById("myButton");
 
     const img = document.createElement("img")
@@ -38,6 +35,37 @@ const button = document.getElementById('#myButton');
 button.addEventListener("click", () => {
     appendImage('Assets/carPark.jpg')
   
+})*/
+//
+
+const guestCount = document.querySelectorAll(".guests")
+guestCount.forEach((guests) => {
+
+const subtractBtn = document.querySelector(".subtract");
+const addtBtn = document.querySelector(".add");
+const reservationValue = document.querySelector(".reservationValue");
+
+//initialize a variable to store the count
+let count = 0;
+//function to update the value
+
+function updateValue() {
+    reservationValue.value = count
+}
+
+//
+subtractBtn.addEventListener("click", () => {
+    if (count > 0){
+        count--;
+        updateValue()
+        console.log(count)
+    }
 })
 
-
+addtBtn.addEventListener("click", () => {
+        count++;
+        updateValue()
+        console.log(count)
+   
+})
+})
